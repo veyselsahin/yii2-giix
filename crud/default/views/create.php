0ccf1731a6fede431d274c26440f449bbc381f99
Veysel Şahin <vshn@windowslive.com>
2015-08-03 19:04:10 +0300

    second commit

---
+++ crud/default/views/create.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/**
+ * @var yii\web\View $this
+ * @var yii\gii\generators\crud\Generator $generator
+ */
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+
+/**
+* @var yii\web\View $this
+* @var <?= ltrim($generator->modelClass, '\\') ?> $model
+*/
+
+$this->title = 'Oluştur';
+$this->params['breadcrumbs'][] = ['label' => '<?= Inflector::pluralize(
+    Inflector::camel2words(StringHelper::basename($generator->modelClass))
+) ?>', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass), '-', true) ?>-create">
+
+    <p class="pull-left">
+        <?= "<?= " ?>Html::a('İptal', \yii\helpers\Url::previous(), ['class' => 'btn btn-default']) ?>
+    </p>
+    <div class="clearfix"></div>
+
+    <?= "<?php " ?>echo $this->render('_form', [
+    'model' => $model,
+    ]); ?>
+
+</div>
---
+++ crud/default/views/update.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/**
+ * @var yii\web\View $this
+ * @var yii\gii\generators\crud\Generator $generator
+ */
+
+$urlParams = $generator->generateUrlParams();
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+
+/**
+ * @var yii\web\View $this
+ * @var <?= ltrim($generator->modelClass, '\\') ?> $model
+ */
+
+$this->title = '<?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?> Güncelle ' . $model-><?= $generator->getNameAttribute() ?> . '';
+$this->params['breadcrumbs'][] = ['label' => '<?= Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => (string)$model-><?= $generator->getNameAttribute() ?>, 'url' => ['view', <?= $urlParams ?>]];
+$this->params['breadcrumbs'][] = 'Edit';
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass),'-', true) ?>-update">
+
+    <p>
+        <?= "<?= " ?>Html::a('<span class="glyphicon glyphicon-eye-open"></span> Görüntüle', ['view', <?= $urlParams ?>], ['class' => 'btn btn-info']) ?>
+    </p>
+
+	<?= "<?php " ?>echo $this->render('_form', [
+		'model' => $model,
+	]); ?>
+
+</div>
